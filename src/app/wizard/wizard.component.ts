ae1c7767e0a213a5c37a4c5de95a764ec4cdd9cc
Eddie Maas <EDease@MacBook-Pro-2.local>
2017-01-25 15:11:27 +0100

    xml loader 1/2

--- src/app/wizard/wizard.component.ts
+++ src/app/wizard/wizard.component.ts
@@ -10,6 +10,14 @@ import {
 })
 export class WizardComponent implements OnInit {
 
+
+  // trying to load some XML
+  file: File;
+  myReader: FileReader ;
+   
+
+
+
   powers = ['Really Smart', 'Super Flexible',
     'Super Hot', 'Weather Changer'
   ];
@@ -25,9 +33,32 @@ export class WizardComponent implements OnInit {
   ngOnInit() {}
 
 
+  // NOTE 2DO in the parseXML, check if something comes out
+  // NOTE important HTML5 browser needed
+  handleXMLChange ($event) {
+    console.log('Attempting to load XML');
+    this.parseXML($event.target);
+  }
+
+   parseXML(inputValue: any) : void {
+    this.file = inputValue.files[0]; 
+    this.myReader = new FileReader();
+
+    this.myReader.onloadend = function(e){
+      // you can perform an action with readed data here
+      // TODO so this one get stored in "result"
+      console.log(e.target);
+    }
+
+    this.myReader.readAsText(this.file);
+  }
+
+
+
+
   clicked() {
     this.submitted = true;
-    console.log("form submit clicked");
+    console.log('form submit clicked');
   }
   // TODO: Remove this when we're done
   get diagnostic() {
